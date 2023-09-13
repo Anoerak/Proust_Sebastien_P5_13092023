34c70a3c1e3dba785fe1c7a5abc954f9b00844ca
Anørak <seb@iamseb.dev>
2023-09-13 16:05:19 +0200

    We add a header and a footer and some style and images

---
+++ templates/components/footer/footer.php
@@ -0,0 +1,32 @@
+<footer>
+	<div class="website">
+		<h2>Website</h2>
+		<ul>
+			<li>-<a href="index.php?page=home">Home</a></li>
+			<li>-<a href="index.php?page=blog">Blog</a></li>
+			<li>-<a href="index.php?page=about">About</a></li>
+			<li>-<a href="index.php?page=contact">Contact</a></li>
+		</ul>
+	</div>
+	<div class="social">
+		<h2>Follow me</h2>
+		<ul>
+			<li><a href="https://www.facebook.com/"><img src="../../../src/lib/imgs/github.webp"
+						alt="Letter F in white on blue background" />Github</a></li>
+			<li><a href="https://www.instagram.com/"><img src="../../../src/lib/imgs/gitlab.webp"
+						alt="White minimal camera logo on Reddish rainbow background" />GitLab</a></li>
+			<li><a href="https://www.twitter.com/"><img src="../../../src/lib/imgs/Linkedin_logo.webp"
+						alt="White X letter on dirty black background" />Linkedin</a></li>
+		</ul>
+	</div>
+	<div class="newsletter">
+		<h2>Sign up for the newsletter</h2>
+		<p>Be the first to know about the latests news and projects on this awesome blog.</p>
+		<form action="index.php?page=newsletter&action=subscribe" method="post">
+			<input type="text" name="email" placeholder="Your email" class="newsletter__input">
+			<input type="submit" value="Subscribe" class="main__button">
+		</form>
+		<p class="privacy__policy_link">We care about your data in our <a href="index.php?page=privacyPolicy">privacy
+				policy</a></p>
+	</div>
+</footer>
