bdfb9fcb5a4db6dc2e222827d8fc74333b82ad3b
Anørak <seb@iamseb.dev>
2023-09-21 22:17:35 +0200

    Pursuing the platine Medal

--- templates/home/home.php
+++ templates/home/home.php
@@ -1,189 +1,187 @@
- <section class="hero__container">
- 	<h1 class="hero__title">Sébastien P. - Junior Full-Stack Developer</h1>
- 	<div class="cards__container">
- 		<div class="profile__card__main__container">
- 			<div class="profile__avatar">
- 				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 185 196" aria-hidden="true" class="profile__background">
- 					<defs>
- 						<path id="0cdfbf55a4f6da8df6561d7ad9023ce8b" d="M0 0h185v196H0z"></path>
- 						<path d="M133.685 175C162.38 175 185 153.618 185 127.246c0-19.838-11.668-36.944-29.884-45.021C154.997 40.173 124.636 10 85.823 10c-25.36 0-43.575 13.305-55.243 29.341-23.336-6.058-48.457 11.285-49.172 37.776C-40.022 80.917-53 100.28-53 123.327c0 27.796 24.526 51.554 57.149 51.554l129.536.119z" id="0cdfbf55a4f6da8df6561d7ad9023ce8e"></path>
- 						<linearGradient x1="19.823%" y1="45.17%" x2="-1.128%" y2="45.17%" id="0cdfbf55a4f6da8df6561d7ad9023ce8a">
- 							<stop stop-color="#FFF" offset="0%"></stop>
- 							<stop stop-color="#FFF" stop-opacity="0" offset="100%"></stop>
- 						</linearGradient>
- 						<filter x="-4.6%" y="-6.7%" width="109.2%" height="113.3%" filterUnits="objectBoundingBox" id="0cdfbf55a4f6da8df6561d7ad9023ce8d">
- 							<feGaussianBlur stdDeviation="6.5" in="SourceAlpha" result="shadowBlurInner1">
- 							</feGaussianBlur>
- 							<feOffset dy="9" in="shadowBlurInner1" result="shadowOffsetInner1"></feOffset>
- 							<feComposite in="shadowOffsetInner1" in2="SourceAlpha" operator="arithmetic" k2="-1" k3="1" result="shadowInnerInner1"></feComposite>
- 							<feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.133002328 0" in="shadowInnerInner1"></feColorMatrix>
- 						</filter>
- 					</defs>
- 					<g fill="none" fill-rule="evenodd">
- 						<mask id="0cdfbf55a4f6da8df6561d7ad9023ce8c" fill="url(#0cdfbf55a4f6da8df6561d7ad9023ce8a)">
- 							<use xlink:href="#0cdfbf55a4f6da8df6561d7ad9023ce8b"></use>
- 						</mask>
- 						<g fill-rule="nonzero" mask="url(#0cdfbf55a4f6da8df6561d7ad9023ce8c)" fill="#000">
- 							<use filter="url(#0cdfbf55a4f6da8df6561d7ad9023ce8d)" xlink:href="#0cdfbf55a4f6da8df6561d7ad9023ce8e"></use>
- 						</g>
- 					</g>
- 				</svg>
- 				<a href="index.php?page=about" class="static__avatar">
- 					<div class="profile__image__container">
- 						<img alt="Your profile Image" src="../../src/lib/imgs/geek.webp">
- 					</div>
- 				</a>
- 				<a href="../../public/files/Resume_Seb_classic.pdf" class="resume__logo" target=”_blank” rel="noopener noreferrer">
- 					<img src="../../src/lib/imgs/cv.png" alt="Drawinf of a resume" />
- 				</a>
- 			</div>
- 			<div class="user__info" aria-hidden="true">
- 				<div class="user-name">Sébastien</div>
- 				<a href="index.php?page=contact" class="email">seb@iamseb.dev</a>
- 			</div>
- 			<div class="plan__name" aria-hidden="true">Open to Work</div>
- 		</div>
+<section class="hero__container">
+	<h1 class="hero__title">Sébastien P. - Junior Full-Stack Developer</h1>
+	<div class="cards__container">
+		<div class="profile__card__main__container">
+			<div class="profile__avatar">
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 185 196" aria-hidden="true" class="profile__background">
+					<defs>
+						<path id="0cdfbf55a4f6da8df6561d7ad9023ce8b" d="M0 0h185v196H0z"></path>
+						<path d="M133.685 175C162.38 175 185 153.618 185 127.246c0-19.838-11.668-36.944-29.884-45.021C154.997 40.173 124.636 10 85.823 10c-25.36 0-43.575 13.305-55.243 29.341-23.336-6.058-48.457 11.285-49.172 37.776C-40.022 80.917-53 100.28-53 123.327c0 27.796 24.526 51.554 57.149 51.554l129.536.119z" id="0cdfbf55a4f6da8df6561d7ad9023ce8e"></path>
+						<linearGradient x1="19.823%" y1="45.17%" x2="-1.128%" y2="45.17%" id="0cdfbf55a4f6da8df6561d7ad9023ce8a">
+							<stop stop-color="#FFF" offset="0%"></stop>
+							<stop stop-color="#FFF" stop-opacity="0" offset="100%"></stop>
+						</linearGradient>
+						<filter x="-4.6%" y="-6.7%" width="109.2%" height="113.3%" filterUnits="objectBoundingBox" id="0cdfbf55a4f6da8df6561d7ad9023ce8d">
+							<feGaussianBlur stdDeviation="6.5" in="SourceAlpha" result="shadowBlurInner1">
+							</feGaussianBlur>
+							<feOffset dy="9" in="shadowBlurInner1" result="shadowOffsetInner1"></feOffset>
+							<feComposite in="shadowOffsetInner1" in2="SourceAlpha" operator="arithmetic" k2="-1" k3="1" result="shadowInnerInner1"></feComposite>
+							<feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.133002328 0" in="shadowInnerInner1"></feColorMatrix>
+						</filter>
+					</defs>
+					<g fill="none" fill-rule="evenodd">
+						<mask id="0cdfbf55a4f6da8df6561d7ad9023ce8c" fill="url(#0cdfbf55a4f6da8df6561d7ad9023ce8a)">
+							<use xlink:href="#0cdfbf55a4f6da8df6561d7ad9023ce8b"></use>
+						</mask>
+						<g fill-rule="nonzero" mask="url(#0cdfbf55a4f6da8df6561d7ad9023ce8c)" fill="#000">
+							<use filter="url(#0cdfbf55a4f6da8df6561d7ad9023ce8d)" xlink:href="#0cdfbf55a4f6da8df6561d7ad9023ce8e"></use>
+						</g>
+					</g>
+				</svg>
+				<a href="index.php?page=about" class="static__avatar">
+					<div class="profile__image__container">
+						<img alt="Your profile Image" src="../../src/lib/imgs/geek.webp">
+					</div>
+				</a>
+				<a href="../../public/files/Resume_Seb_classic.pdf" class="resume__logo" target=”_blank” rel="noopener noreferrer">
+					<img src="../../src/lib/imgs/cv.png" alt="Drawing of a resume" />
+				</a>
+			</div>
+			<div class="user__info" aria-hidden="true">
+				<div class="user-name">Sébastien</div>
+				<a href="index.php?page=contact" class="email">seb@iamseb.dev</a>
+			</div>
+			<div class="plan__name" aria-hidden="true">Open to Work</div>
+		</div>
 
- 		<div class="card__techno__container">
- 			<div class="technos">
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/html5.webp" alt="" />
- 					</span>
- 					<p>HTML5</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/css3.webp" alt="" />
- 					</span>
- 					<p>CSS3</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/js.webp" alt="" />
- 					</span>
- 					<p>JS</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/php.webp" alt="" />
- 					</span>
- 					<p>PHP</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/react.webp" alt="" />
- 					</span>
- 					<p>React</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/vue.webp" alt="" />
- 					</span>
- 					<p>Vue</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/symfony.webp" alt="" />
- 					</span>
- 					<p>Symfony</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/node.webp" alt="" />
- 					</span>
- 					<p>Node</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/github.webp" alt="" />
- 					</span>
- 					<p>GitHub</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/gitlab.webp" alt="" />
- 					</span>
- 					<p>GitLab</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/git.webp" alt="" />
- 					</span>
- 					<p>Git</p>
- 				</div>
- 				<div class="techno">
- 					<span>
- 						<img src="../../src/lib/imgs/technos/figma.webp" alt="" />
- 					</span>
- 					<p>Figma</p>
- 				</div>
- 			</div>
- 		</div>
+		<div class="card__techno__container">
+			<div class="technos">
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/html5.webp" alt="" />
+					</span>
+					<p>HTML5</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/css3.webp" alt="" />
+					</span>
+					<p>CSS3</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/js.webp" alt="" />
+					</span>
+					<p>JS</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/php.webp" alt="" />
+					</span>
+					<p>PHP</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/react.webp" alt="" />
+					</span>
+					<p>React</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/vue.webp" alt="" />
+					</span>
+					<p>Vue</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/symfony.webp" alt="" />
+					</span>
+					<p>Symfony</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/node.webp" alt="" />
+					</span>
+					<p>Node</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/github.webp" alt="" />
+					</span>
+					<p>GitHub</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/gitlab.webp" alt="" />
+					</span>
+					<p>GitLab</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/git.webp" alt="" />
+					</span>
+					<p>Git</p>
+				</div>
+				<div class="techno">
+					<span>
+						<img src="../../src/lib/imgs/technos/figma.webp" alt="" />
+					</span>
+					<p>Figma</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
+<section class="summary__container">
+	<div class="drawer__container">
 
- 	</div>
- </section>
+		<article class="about">
+			<a href="index.php?page=about">
+				<h2>The Dev</h2>
+			</a>
+			<div class="headmoji">
+				👨‍👩‍👧 🧘🏼‍♂️ 🧑🏼‍💻 👾 🥾 ✈️ 📚
+			</div>
+			<p class="about__me">
+				👋 Hi, <br />
+				I’m Sébastien 😃, a tech enthusiast since childhood.
+				I transitioned from a National Sales Manager to a full-stack developer 🧑🏼‍💻 in 2022. <br />
+				I’m skilled in HTML, CSS, JavaScript, PHP, React, Redux, Vue, Symfony, and more. <br />
+				I often contribute to open-source projects using Python and C++. <br />
+				I’m seeking opportunities in Sweden 🇸🇪 where I plan to relocate with my family 👨‍👩‍👧. <br />
+				Apart from coding, I enjoy being a father 👨, yoga 🧘🏼‍♂️ (I’m a teacher too), hiking 🥾, and gaming
+				👾. <br />
+				Let’s connect if you’re looking for dedication and value in your projects.
+			</p>
+			<div class="bottom">
+				<span class="storage">38y. of Storage</span>
+				<span class="shared">Shared by Sebastien (Professional Sharing)</span>
+			</div>
 
- <section class="summary__container">
- 	<div class="drawer__container">
+		</article>
 
- 		<article class="about">
- 			<a href="index.php?page=about">
- 				<h2>The Dev</h2>
- 			</a>
- 			<div class="headmoji">
- 				👨‍👩‍👧 🧘🏼‍♂️ 🧑🏼‍💻 👾 🥾 ✈️ 📚
- 			</div>
- 			<p class="about__me">
- 				👋 Hi, <br />
- 				I’m Sébastien 😃, a tech enthusiast since childhood.
- 				I transitioned from a National Sales Manager to a full-stack developer 🧑🏼‍💻 in 2022. <br />
- 				I’m skilled in HTML, CSS, JavaScript, PHP, React, Redux, Vue, Symfony, and more. <br />
- 				I often contribute to open-source projects using Python and C++. <br />
- 				I’m seeking opportunities in Sweden 🇸🇪 where I plan to relocate with my family 👨‍👩‍👧. <br />
- 				Apart from coding, I enjoy being a father 👨, yoga 🧘🏼‍♂️ (I’m a teacher too), hiking 🥾, and gaming
- 				👾. <br />
- 				Let’s connect if you’re looking for dedication and value in your projects.
- 			</p>
- 			<div class="bottom">
- 				<span class="storage">38y. of Storage</span>
- 				<span class="shared">Shared by Sebastien (Professional Sharing)</span>
- 			</div>
+		<article class="projects">
+			<a href="index.php?page=blog&option=all">
+				<h2>The Projects</h2>
+			</a>
+			<img src="https://github-readme-stats-git-main-anoerak.vercel.app/api/top-langs/?username=Anoerak&layout=compact&langs_count=8&hide=html" />
+			<p class="projects__speech">
+				All my projects aren't deployed yet, but you can find them on my GitHub <img src="../../src/lib/imgs/github.svg" alt="Cat outline" /> account.
+				I realized a lot of them during my training at OpenClassrooms <img src="../../src/lib/imgs/OCR_logo.webp" alt="Reversed white C on violet background">, but also
+				during my free time.
+			</p>
+			<div class="bottom">
+				<span class="projects__completed">33 Projects Completed</span>
+				<span class="career">??% of my career</span>
+			</div>
+		</article>
 
- 		</article>
+		<article class="contact">
+			<a href="index.php?page=contact">
+				<h2>The Connection</h2>
+			</a>
+			<div class="head__icons">
+				<img src="../../src/lib/imgs/gmail.webp" alt="" />
+				<img src="../../src/lib/imgs/outlook.webp" alt="" />
+				<img src="../../src/lib/imgs/mail.webp" alt="" />
+				<img src="../../src/lib/imgs/proton.webp" alt="" />
+				<img src="../../src/lib/imgs/mailchimp.webp" alt="" />
+			</div>
+			<div class="bottom">
+				<span class="contact__note">Connect, ask, cheer by using the form.</span>
+			</div>
 
- 		<article class="projects">
- 			<a href="index.php?page=blog&option=all">
- 				<h2>The Projects</h2>
- 			</a>
- 			<img src="https://github-readme-stats-git-main-anoerak.vercel.app/api/top-langs/?username=Anoerak&layout=compact&langs_count=8&hide=html" />
- 			<p class="projects__speech">
- 				All my projects aren't deployed yet, but you can find them on my GitHub <img src="../../src/lib/imgs/github.svg" alt="Cat outline" /> account.
- 				I realized a lot of them during my training at OpenClassrooms <img src="../../src/lib/imgs/OCR_logo.webp" alt="Reversed white C on violet background">, but also
- 				during my free time.
- 			</p>
- 			<div class="bottom">
- 				<span class="projects__completed">33 Projects Completed</span>
- 				<span class="career">??% of my career</span>
- 			</div>
- 		</article>
-
- 		<article class="contact">
- 			<a href="index.php?page=contact">
- 				<h2>The Connection</h2>
- 			</a>
- 			<div class="head__icons">
- 				<img src="../../src/lib/imgs/gmail.webp" alt="" />
- 				<img src="../../src/lib/imgs/outlook.webp" alt="" />
- 				<img src="../../src/lib/imgs/mail.webp" alt="" />
- 				<img src="../../src/lib/imgs/proton.webp" alt="" />
- 				<img src="../../src/lib/imgs/mailchimp.webp" alt="" />
- 			</div>
- 			<div class="bottom">
- 				<span class="contact__note">Connect, ask, cheer by using the form.</span>
- 			</div>
-
- 		</article>
- 	</div>
- </section>
+		</article>
+	</div>
+</section>
